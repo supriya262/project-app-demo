a7d9661d086b278f9730d62151dabc0712417a67
supriya mishra <sachisupriya65@gmail.com>
2021-06-06 15:23:45 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
+
+
 import Menu from './Menu';
-import Dashboard from './Dashboard';
-import Footer from './Footer';
 
 <script src="/assets/vendor/chart.js/dist/Chart.extension.js"></script>
 function App() {
   return (
     <div class="wrapper">
-      <Header/>
+    
+      
+      
       <Menu/>
       
-      <Dashboard/>
-      <Footer/>
       
     </div>
   );
